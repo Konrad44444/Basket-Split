f86b60cab88d7d139dad1a71c0e3622fa8f14433
Konrad44444 <konrad_warzecha@wp.pl>
2024-04-06 13:26:33 +0200

    read data to map

--- src/main/java/com/ocado/basket/BasketSplitter.java
+++ src/main/java/com/ocado/basket/BasketSplitter.java
@@ -4,6 +4,7 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
@@ -11,12 +12,13 @@ import java.util.Map;
 import java.util.Set;
 
 import org.apache.commons.io.IOUtils;
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 public class BasketSplitter {
 
-    Map<String, List<String>> deliveryProducts = new HashMap<>();
+    Map<String, List<String>> deliveryTypesAndProducts = new HashMap<>();
 
     public BasketSplitter(String absolutePathToConfigFile) {
         String jsonTxt = "";
@@ -40,7 +42,20 @@ public class BasketSplitter {
             Set<String> keys = config.keySet();
 
             for (String key : keys) {
-                System.out.println(key);
+                // key is a prduct, which has one or many delivery types
+                JSONArray deliveryTypes = config.getJSONArray(key);
+
+                // for each delivery type add an product
+                for (Object deliveryType : deliveryTypes) {
+
+                    String dType = (String) deliveryType;
+
+                    if (deliveryTypesAndProducts.containsKey(dType)) {
+                        deliveryTypesAndProducts.get(dType).add(key);
+                    } else {
+                        deliveryTypesAndProducts.put(dType, new ArrayList<>(List.of(key)));
+                    }
+                }
             }
 
         } catch (JSONException e) {
@@ -50,5 +65,21 @@ public class BasketSplitter {
 
     public Map<String, List<String>> split(List<String> items) {
         return Collections.emptyMap();
+    }
+
+    public String toString() {
+        StringBuilder result = new StringBuilder();
+
+        for (String key : deliveryTypesAndProducts.keySet()) {
+            result.append("Delivery type: " + key + "\n");
+
+            for (String product : deliveryTypesAndProducts.get(key)) {
+                result.append(product + ", ");
+            }
+
+            result.append("\n\n-----\n");
+        }
+
+        return result.toString();
     }
 }
